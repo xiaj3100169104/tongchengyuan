39cf89636196a6822bcc16596d0365c4d69e49d4
kunkka <kunkkawang2016@gmail.com>
2017-05-26 15:19:15 +0800

    完成了上传用户属性的操作。

--- app/src/main/java/com/juns/wechat/net/request/HttpActionImpl.java
+++ app/src/main/java/com/juns/wechat/net/request/HttpActionImpl.java
@@ -1,6 +1,7 @@
 package com.juns.wechat.net.request;
 
 import com.juns.wechat.bean.CommentBean;
+import com.juns.wechat.bean.UserPropertyBean;
 import com.juns.wechat.config.ConfigUtil;
 import com.juns.wechat.manager.AccountManager;
 import com.style.net.core.HttpActionManager;
@@ -9,6 +10,7 @@ import com.style.net.core.NetDataBeanCallback;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -73,7 +75,7 @@ public class HttpActionImpl {
     private OkHttpClient getClient() {
         OkHttpClient.Builder client = new OkHttpClient.Builder()
                 //添加请求拦截
-                .connectTimeout(5, TimeUnit.SECONDS)//设置超时时间
+                //.connectTimeout(5, TimeUnit.SECONDS)//设置超时时间
                 .retryOnConnectionFailure(true)
                 .addInterceptor(new Interceptor() {
                     @Override
@@ -123,12 +125,13 @@ public class HttpActionImpl {
         runTask(tag, call, callback);
     }
 
-    public void updateUserProperty(String tag, String userProperties, NetDataBeanCallback callback) {
+    public void updateUserProperty(String tag, String value, NetDataBeanCallback callback) {
         /*RequestBody requestBody = new MultipartBody.Builder()
                 //.setType(MultipartBody.FORM)
                 .addFormDataPart("userProperties", userProperties)
                 .build();*/
-        Call<String> call = serviceLocal.updateUserProperty(userProperties);
+        RequestBody requestBody = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), value);
+        Call<String> call = service.updateUserProperty(requestBody);
         runTask(tag, call, callback);
     }
 
